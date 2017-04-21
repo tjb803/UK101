e1471abd7c8cb2fc45163e1c0990dde005bb2899
tjb803 <tim.baldwin@tinymail.co.uk>
2017-04-21 19:47:54 +0000

    Improve tape file selection for Mac

--- src/uk101/view/component/TapeFormat.java
+++ src/uk101/view/component/TapeFormat.java
@@ -5,12 +5,16 @@
  */
 package uk101.view.component;
 
+import java.awt.Insets;
+
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.border.TitledBorder;
 
 import uk101.io.Tape;
 
@@ -31,50 +35,74 @@ public class TapeFormat extends JPanel {
 
     public TapeFormat() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setBorder(BorderFactory.createTitledBorder("Tape Format"));
 
-        MiniPanel ip = new MiniPanel();
-        ip.setLayout(new BoxLayout(ip, BoxLayout.Y_AXIS));
-        ip.setBorder(BorderFactory.createTitledBorder("Input"));
-        iAuto = new JRadioButton(MODE_AUTO, true);
-        JRadioButton ia = new JRadioButton(MODE_ASCII);
-        JRadioButton ib = new JRadioButton(MODE_BINARY);
-        JRadioButton iw = new JRadioButton(MODE_AUDIO);
+        iAuto = new JRadioButton((String)null, true);
+        JRadioButton ia = new JRadioButton();
+        JRadioButton ib = new JRadioButton();
+        JRadioButton iw = new JRadioButton();
         iAuto.setActionCommand(Integer.toString(Tape.STREAM_SELECT));
         ia.setActionCommand(Integer.toString(Tape.STREAM_ASCII));
         ib.setActionCommand(Integer.toString(Tape.STREAM_BINARY));
         iw.setActionCommand(Integer.toString(Tape.STREAM_AUDIO));
         inputGroup = new ButtonGroup();
         inputGroup.add(iAuto);  inputGroup.add(ia);  inputGroup.add(ib);  inputGroup.add(iw);
-        ip.add(iAuto); 
-        ip.add(ia); 
-        ip.add(ib);
-        ip.add(iw);
 
-        MiniPanel op = new MiniPanel();
-        op.setLayout(new BoxLayout(op, BoxLayout.Y_AXIS));
-        op.setBorder(BorderFactory.createTitledBorder("Output"));
-        oAscii = new JRadioButton(MODE_ASCII, true);
-        oAscii.setMaximumSize(iAuto.getMaximumSize());
-        JRadioButton ob = new JRadioButton(MODE_BINARY);
-        JRadioButton ow = new JRadioButton(MODE_AUDIO);
+        oAscii = new JRadioButton((String)null, true);
+        JRadioButton ob = new JRadioButton();
+        JRadioButton ow = new JRadioButton();
         oAscii.setActionCommand(Integer.toString(Tape.STREAM_ASCII));
         ob.setActionCommand(Integer.toString(Tape.STREAM_BINARY));
         ow.setActionCommand(Integer.toString(Tape.STREAM_AUDIO));
         outputGroup = new ButtonGroup();
         outputGroup.add(oAscii);  outputGroup.add(ob);  outputGroup.add(ow);
-        op.add(oAscii); 
-        op.add(ob); 
-        op.add(ow);
 
-        add(ip);
-        Box.createVerticalStrut(3);
-        add(op);
+        JPanel bp = new JPanel();
+        bp.setLayout(new BoxLayout(bp, BoxLayout.Y_AXIS));
+        TitledBorder hb = BorderFactory.createTitledBorder("Tape Format");
+        hb.setTitleJustification(TitledBorder.CENTER);
+        hb.setTitlePosition(TitledBorder.ABOVE_TOP);
+        bp.setBorder(hb);
+        
+        JPanel heads = new JPanel();
+        heads.setLayout(new BoxLayout(heads, BoxLayout.X_AXIS));
+        heads.add(new JLabel("Read"));
+        heads.add(Box.createHorizontalGlue());
+        heads.add(new JLabel("Write"));
+        bp.add(heads);
+        bp.add(Box.createVerticalStrut(5));
+        bp.add(buttonPair(MODE_AUTO, iAuto, null));
+        bp.add(buttonPair(MODE_ASCII, ia, oAscii));
+        bp.add(buttonPair(MODE_BINARY, ib, ob));
+        bp.add(buttonPair(MODE_AUDIO, iw, ow));
+        
+        add(bp);
+    }
+    
+    private JPanel buttonPair(String bt, JRadioButton bl, JRadioButton br) {
+        JPanel bp = new JPanel();
+        bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
+        bl.setIconTextGap(0);
+        bl.setMargin(new Insets(2, 0, 2, 10));
+        bp.add(bl);
+        bp.add(Box.createHorizontalGlue()); 
+        bp.add(new JLabel(bt));
+        bp.add(Box.createHorizontalGlue());
+        if (br != null) {
+            br.setIconTextGap(0);
+            br.setMargin(new Insets(2, 10, 2, 0));
+            bp.add(br);
+        } else {
+            JLabel fill = new JLabel();   
+            fill.setMaximumSize(bl.getPreferredSize());
+            fill.setPreferredSize(bl.getPreferredSize());
+            bp.add(fill);
+        }
+        return bp;
     }
     
     public void reset() {
-      iAuto.setEnabled(true);
-      oAscii.setEnabled(true);
+      iAuto.setSelected(true);
+      oAscii.setSelected(true);
     }
 
     public int getInputFormat() {
